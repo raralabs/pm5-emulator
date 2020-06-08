209de1a80d9f458ec04a8b991abfdba1bac9224c
n-is <nschl.npl@gmail.com>
2020-06-08 22:27:08 +0545

    Add decorator interface and boilerplate code.

---
+++ service/decorator/interface.go
@@ -0,0 +1,128 @@
+package decorator
+
+import "github.com/bettercap/gatt"
+
+// IServiceDecorator wraps a gatt service.
+type IServiceDecorator interface {
+	// AddCharacteristic adds a characteristic to a service.
+	// AddCharacteristic panics if the service already contains another
+	// characteristic with the same UUID.
+	AddCharacteristic(u gatt.UUID) *gatt.Characteristic
+
+	// UUID returns the UUID of the service.
+	UUID() gatt.UUID
+
+	// Name returns the specification name of the service according to its UUID.
+	// If the UUID is not assignee, Name returns an empty string.
+	Name() string
+
+	// Handle returns the Handle of the service.
+	Handle() uint16
+
+	// EndHandle returns the End Handle of the service.
+	EndHandle() uint16
+
+	// SetHandle sets the Handle of the service.
+	SetHandle(h uint16)
+
+	// SetEndHandle sets the End Handle of the service.
+	SetEndHandle(endh uint16)
+
+	// SetCharacteristics sets the Characteristics of the service.
+	SetCharacteristics(chars []*gatt.Characteristic)
+
+	// Characteristic returns the contained characteristic of this service.
+	Characteristics() []*gatt.Characteristic
+}
+
+
+// IServiceDecorator wraps a gatt characteristics.
+type ICharacteristicDecorator interface {
+
+	// UUID returns the UUID of the characteristic.
+	UUID() gatt.UUID
+
+	// Name returns the specification name of the characteristic according to its UUID.
+	// If the UUID is not assignee, Name returns an empty string.
+	Name() string
+
+	// Handle returns the Handle of the characteristic.
+	Handle() uint16
+
+	// VHandle returns the Value Handle of the characteristic.
+	VHandle() uint16
+
+	// EndHandle returns the End Handle of the characteristic.
+	EndHandle() uint16
+
+	// Descriptor returns the Descriptor of the characteristic.
+	Descriptor() *gatt.Descriptor
+
+	// SetHandle sets the Handle of the characteristic.
+	SetHandle(h uint16)
+
+	// SetVHandle sets the Value Handle of the characteristic.
+	SetVHandle(vh uint16)
+
+	// SetEndHandle sets the End Handle of the characteristic.
+	SetEndHandle(endh uint16)
+
+	// SetDescriptor sets the Descriptor of the characteristic.
+	SetDescriptor(cccd *gatt.Descriptor)
+
+	// SetDescriptors sets the list of Descriptor of the characteristic.
+	SetDescriptors(descs []*gatt.Descriptor)
+
+	// Service returns the containing service of this characteristic.
+	Service() *gatt.Service
+
+	// Properties returns the properties of this characteristic.
+	Properties() gatt.Property
+
+	// Descriptors returns the contained descriptors of this characteristic.
+	Descriptors() []*gatt.Descriptor
+
+	// AddDescriptor adds a descriptor to a characteristic.
+	// AddDescriptor panics if the characteristic already contains another
+	// descriptor with the same UUID.
+	AddDescriptor(u gatt.UUID) *gatt.Descriptor
+
+	// SetValue makes the characteristic support read requests, and returns a
+	// static value. SetValue must be called before the containing service is
+	// added to a server.
+	// SetValue panics if the characteristic has been configured with a ReadHandler.
+	SetValue(b []byte)
+
+	// HandleRead makes the characteristic support read requests, and routes read
+	// requests to h. HandleRead must be called before the containing service is
+	// added to a server.
+	// HandleRead panics if the characteristic has been configured with a static value.
+	HandleRead(h gatt.ReadHandler)
+
+	// HandleReadFunc calls HandleRead(ReadHandlerFunc(f)).
+	HandleReadFunc(f func(rsp gatt.ResponseWriter, req *gatt.ReadRequest))
+
+	GetReadHandler() gatt.ReadHandler
+
+	// HandleWrite makes the characteristic support write and write-no-response
+	// requests, and routes write requests to h.
+	// The WriteHandler does not differentiate between write and write-no-response
+	// requests; it is handled automatically.
+	// HandleWrite must be called before the containing service is added to a server.
+	HandleWrite(h gatt.WriteHandler)
+
+	// HandleWriteFunc calls HandleWrite(WriteHandlerFunc(f)).
+	HandleWriteFunc(f func(r gatt.Request, data []byte) (status byte))
+
+	GetWriteHandler() gatt.WriteHandler
+
+	// HandleNotify makes the characteristic support notify requests, and routes
+	// notification requests to h. HandleNotify must be called before the
+	// containing service is added to a server.
+	HandleNotify(h gatt.NotifyHandler)
+
+	// HandleNotifyFunc calls HandleNotify(NotifyHandlerFunc(f)).
+	HandleNotifyFunc(f func(r gatt.Request, n gatt.Notifier))
+
+	GetNotifyHandler() gatt.NotifyHandler
+}
---
+++ service/decorator/subscriber.go
@@ -0,0 +1,183 @@
+package decorator
+
+import (
+	"github.com/bettercap/gatt"
+)
+
+/*
+ * Service Subscriber
+ */
+type ServiceSubscriber struct {
+	service *gatt.Service	// service that this decorator wraps
+}
+
+func NewServiceSubscriber(service *gatt.Service) *ServiceSubscriber {
+	return &ServiceSubscriber{service: service}
+}
+
+
+/** Functionalities Added **/
+
+func (s *ServiceSubscriber) AddCharacteristic(uuid gatt.UUID) ICharacteristicDecorator {
+	c := s.service.AddCharacteristic(uuid)
+	return NewCharSubscriber(c)
+}
+
+
+/** Functionalities Not Added **/
+
+func (s *ServiceSubscriber) UUID() gatt.UUID {
+	return s.service.UUID()
+}
+
+func (s *ServiceSubscriber) Name() string {
+	return s.service.Name()
+}
+
+func (s *ServiceSubscriber) Handle() uint16 {
+	return s.service.Handle()
+}
+
+func (s *ServiceSubscriber) EndHandle() uint16 {
+	return s.service.EndHandle()
+}
+
+func (s *ServiceSubscriber) SetHandle(h uint16) {
+	s.service.SetHandle(h)
+}
+
+func (s *ServiceSubscriber) SetEndHandle(endh uint16) {
+	s.service.SetEndHandle(endh)
+}
+
+func (s *ServiceSubscriber) SetCharacteristics(chars []*gatt.Characteristic) {
+	s.service.SetCharacteristics(chars)
+}
+
+func (s *ServiceSubscriber) Characteristics() []*gatt.Characteristic {
+	return s.service.Characteristics()
+}
+
+
+/*
+ * Characteristics Subscriber
+ */
+
+type CharSubscriber struct {
+	ch *gatt.Characteristic
+}
+
+func NewCharSubscriber(ch *gatt.Characteristic) *CharSubscriber {
+	return &CharSubscriber{ch: ch}
+}
+
+/** Functionalities Added **/
+
+// Add subscriber workflow:
+// 1. We need to create some client subscriber service
+// 2. Client send notify request to a characteristic to subscribe
+// 3. Whenever a write request arrives at a characteristic, we check whether client has subscribed to notification or
+//    not. If yes, we can notify the client with the results.
+
+func (c *CharSubscriber) HandleReadFunc(f func(rsp gatt.ResponseWriter, req *gatt.ReadRequest)) {
+	c.ch.HandleReadFunc(f)
+}
+
+func (c *CharSubscriber) HandleWriteFunc(f func(r gatt.Request, data []byte) (status byte)) {
+	c.ch.HandleWriteFunc(f)
+}
+
+func (c *CharSubscriber) HandleNotifyFunc(f func(r gatt.Request, n gatt.Notifier)) {
+	c.ch.HandleNotifyFunc(f)
+}
+
+/** Functionalities Not Added **/
+
+func (c *CharSubscriber) UUID() gatt.UUID {
+	return c.ch.UUID()
+}
+
+func (c *CharSubscriber) Name() string {
+	return c.ch.Name()
+}
+
+func (c *CharSubscriber) Handle() uint16 {
+	return c.ch.Handle()
+}
+
+func (c *CharSubscriber) VHandle() uint16 {
+	return c.ch.VHandle()
+}
+
+func (c *CharSubscriber) EndHandle() uint16 {
+	return c.ch.EndHandle()
+}
+
+func (c *CharSubscriber) Descriptor() *gatt.Descriptor {
+	return c.ch.Descriptor()
+}
+
+func (c *CharSubscriber) SetHandle(h uint16) {
+	c.ch.SetHandle(h)
+}
+
+func (c *CharSubscriber) SetVHandle(vh uint16) {
+	c.ch.SetVHandle(vh)
+}
+
+func (c *CharSubscriber) SetEndHandle(endh uint16) {
+	c.ch.SetEndHandle(endh)
+}
+
+func (c *CharSubscriber) SetDescriptor(cccd *gatt.Descriptor) {
+	c.ch.SetDescriptor(cccd)
+}
+
+func (c *CharSubscriber) SetDescriptors(descs []*gatt.Descriptor) {
+	c.ch.SetDescriptors(descs)
+}
+
+func (c *CharSubscriber) Service() *gatt.Service {
+	return c.ch.Service()
+}
+
+func (c *CharSubscriber) Properties() gatt.Property {
+	return c.ch.Properties()
+}
+
+func (c *CharSubscriber) Descriptors() []*gatt.Descriptor {
+	return c.ch.Descriptors()
+}
+
+func (c *CharSubscriber) AddDescriptor(u gatt.UUID) *gatt.Descriptor {
+	return c.ch.AddDescriptor(u)
+}
+
+func (c *CharSubscriber) SetValue(b []byte) {
+	c.ch.SetValue(b)
+}
+
+func (c *CharSubscriber) HandleRead(h gatt.ReadHandler) {
+	c.ch.HandleRead(h)
+}
+
+func (c *CharSubscriber) GetReadHandler() gatt.ReadHandler {
+	return c.ch.GetReadHandler()
+}
+
+func (c *CharSubscriber) HandleWrite(h gatt.WriteHandler) {
+	c.ch.HandleWrite(h)
+}
+
+func (c *CharSubscriber) GetWriteHandler() gatt.WriteHandler {
+	return c.ch.GetWriteHandler()
+}
+
+func (c *CharSubscriber) HandleNotify(h gatt.NotifyHandler) {
+	c.ch.HandleNotify(h)
+}
+
+func (c *CharSubscriber) GetNotifyHandler() gatt.NotifyHandler {
+	return c.ch.GetNotifyHandler()
+}
+
