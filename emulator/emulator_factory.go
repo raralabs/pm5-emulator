5e8345b20b0e6c0a0c98d503d30069960e6ff754
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-08-02 00:38:51 -0400

    use shortcut provided by bettercap/gatt that also defines config for macos

--- emulator/emulator_factory.go
+++ emulator/emulator_factory.go
@@ -5,11 +5,12 @@ import (
 	"pm5-emulator/sm"
 
 	"github.com/bettercap/gatt"
+	"github.com/bettercap/gatt/examples/option"
 )
 
 //NewEmulator factory methods initializes emulator
 func NewEmulator() *Emulator {
-	d, err := gatt.NewDevice(defaultServerOptions...)
+	d, err := gatt.NewDevice(option.DefaultServerOptions...)
 	if err != nil {
 		log.Fatalf("Failed to open config, err: %s", err)
 	}
@@ -17,9 +18,4 @@ func NewEmulator() *Emulator {
 		device:       d,
 		stateMachine: sm.NewStateMachine(),
 	}
-}
-
-//BLE Server Options
-var defaultServerOptions = []gatt.Option{
-	gatt.MacDeviceRole(gatt.PeripheralManager),
 }
