012c153518cbfa04384deab3c46f09999b9a45d8
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-08-02 16:36:53 -0400

    add server config for macos (#22)
    * fix shit
    
    * use shortcut provided by bettercap/gatt that also defines config for macos
    
    * Copy option package example from bettercap/gatt examples
    While this is BSD-3-clause licensed, its basically all configuration anyway
    
    ---------
    
    Co-authored-by: Adrian Edwards <adrian@adriancedwards.com>

--- emulator/emulator_factory.go
+++ emulator/emulator_factory.go
@@ -3,14 +3,15 @@ package emulator
 import (
 	"log"
 	"pm5-emulator/sm"
+	"config/option"
+
 
 	"github.com/bettercap/gatt"
-	"github.com/bettercap/gatt/linux/cmd"
 )
 
 //NewEmulator factory methods initializes emulator
 func NewEmulator() *Emulator {
-	d, err := gatt.NewDevice(defaultServerOptions...)
+	d, err := gatt.NewDevice(option.DefaultServerOptions...)
 	if err != nil {
 		log.Fatalf("Failed to open config, err: %s", err)
 	}
@@ -18,15 +19,4 @@ func NewEmulator() *Emulator {
 		device:       d,
 		stateMachine: sm.NewStateMachine(),
 	}
-}
-
-//BLE Server Options
-var defaultServerOptions = []gatt.Option{
-	gatt.LnxMaxConnections(1),
-	gatt.LnxDeviceID(-1, true),
-	gatt.LnxSetAdvertisingParameters(&cmd.LESetAdvertisingParameters{
-		AdvertisingIntervalMin: 0x00f4,
-		AdvertisingIntervalMax: 0x00f4,
-		AdvertisingChannelMap:  0x7,
-	}),
 }
